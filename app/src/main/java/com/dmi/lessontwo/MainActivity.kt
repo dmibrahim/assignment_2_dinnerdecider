7da6dfe4234b342a5b747a05ab6f4862dd71a7bc
dmibrahim <micky_dcs_faith@yahoo.com>
2022-02-14 17:42:10 -0600

    upload all project files

---
+++ app/src/main/java/com/dmi/lessontwo/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.dmi.lessontwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var menuList = ArrayList<String>(arrayListOf("Hamburger", "Pizza","Mexican", "American", "Chinese"))
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnAdd.setOnClickListener {addDinner()}
+        btnDecide.setOnClickListener{decideDinner()}
+    }
+
+    private fun addDinner(){
+
+        val name = foodNameTxt.text
+        if(name.toString().isEmpty()){
+           Toast.makeText(applicationContext,"please enter a name",Toast.LENGTH_LONG).show()
+            foodNameTxt.requestFocus()
+        }
+        else{
+            menuList.add(name.toString())
+            dinnerText.setText(menuList.get(menuList.size-1))
+            foodNameTxt.text.clear()
+        }
+
+    }
+    private fun decideDinner( ){
+        val random = (0 until menuList.size-1).random()
+        dinnerText.setText(menuList.get(random))
+    }
+
+}
